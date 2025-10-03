3a7cafcfb7e41f30cb7e2c2a041e304607f477f6
Kurmanbek-1 <kurmanbekmomunkulov32@gmail.com>
2025-10-03 18:51:27 +0600

    Lesson 2

---
+++ main.py
@@ -0,0 +1,11 @@
+import flet as ft 
+
+def main(page: ft.Page):
+    page.title = 'Моё первое приложение!'
+
+    greeting_text = ft.Text("Hello world!")
+
+    page.add(greeting_text)
+
+
+ft.app(target=main, view=ft.WEB_BROWSER)
